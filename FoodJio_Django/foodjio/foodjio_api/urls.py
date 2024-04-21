a287103f61622be2e38e24705f8c355627edd26f
Kenneth Koh <kkyz13@gmail.com>
2024-04-21 18:35:48 +0800

    Initial CURD for meet table

--- FoodJio_Django/foodjio/foodjio_api/urls.py
+++ FoodJio_Django/foodjio/foodjio_api/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('helloworld/', views.helloworld.as_view()),
-    path('seedcuisine/', views.seedcuisine.as_view())
+    path('seedcuisine/', views.seedcuisine.as_view()),
+    path('getmeets/', views.get_meets.as_view()),
+    path('getonemeet/<str:pk>/', views.get_one_meet.as_view()),
+    path('addmeet/', views.put_meet.as_view()),
+    path('updatemeet/<str:pk>/',views.patch_meet.as_view()),
+    path('deletemeet/<str:pk>/', views.delete_meet.as_view())
+
 ]
 
